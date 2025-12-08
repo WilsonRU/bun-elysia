7fd7ba794039a482aa4d75715516dae1a9e58226
Wilson Moura <wilo.moura3@gmail.com>
2025-12-08 07:38:01 -0300

    The way Elysia initializes has been changed to be compatible with deployment on Vercel.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,4 @@
 export const config = {
-	port: Number(Bun.env.PORT || 3000),
 	databaseUrl: Bun.env.DATABASE_URL || "postgres://user:password@localhost:5432/mydb",
 	redisUrl: Bun.env.REDIS_URL || "redis://localhost:6379",
 	secret: Bun.env.SECRET || "",
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { createHttp } from "@/adapters/http/elysia";
-import { config } from "@/config/env";
 
 const app = createHttp();
 
-app.listen(config.port, () => {
-	console.log(`🦊 HTTP is running at 0.0.0.0:${config.port}`);
-});
+export default app;
